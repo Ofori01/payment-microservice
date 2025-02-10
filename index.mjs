818ca2a4e317360b04d3abe14e22b08d84b0cf96
Ofori01 <128515949+Ofori01@users.noreply.github.com>
2025-02-10 06:52:17 +0000

    added cors

--- index.mjs
+++ index.mjs
@@ -1,9 +1,11 @@
 import express from "express";
 import paymentRouter from "./routes/paymentRoutes.mjs";
 import mongoose from "mongoose";
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI_PAYMENTS, {
 }).then(() => console.log("Connected to MongoDB"))
